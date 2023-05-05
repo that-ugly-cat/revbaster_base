742575d4710c24cc698753a76327cddd3a27fa32
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-05 18:01:31 +0200

    Create initial_config.py

---
+++ initial_config.py
@@ -0,0 +1,6 @@
+project_title = 'Ethical considerations on social listening and infodemic management'
+project_description = 'Scoping review. info and details [here](https://osf.io/28d73/wiki/home/)'
+inclusion_criteria = '- full text is available AND \n- full text mentions social listening or infodemic management (directly or indirectly, see \‘infodemics (expanded)\’ in query definition/query v3) OR (\n- full text mentions outbreak, epidemic, pandemic OR full text mentions public health, risk for public health, public health emergency, and related concepts (both ‘acute’ and ‘chronic’)) AND \n- full text mentions ethics or ethical aspects AND full text in English. \n\nPapers in languages other than English will not be excluded but saved in a separate folder, at disposal of the EG.'
+criteria = ['Infodemic: Characterization', 'Infodemic management : Characterization', 'Ethical Issues in Infodemics', 'Ethical Issues in Infodemic management', 'Aims of Infodemic management', 'Conclusions and recommendations']
+firestore_collection = 'papers-WHO-base'
+firestore_collection_users = 'papers-WHO-base_users'
