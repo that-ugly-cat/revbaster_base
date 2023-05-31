b8568e9ca6d208f668c212d89d3ea67e1da3676e
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-31 09:15:07 +0200

    Update revmaster.py

--- revmaster.py
+++ revmaster.py
@@ -583,6 +583,13 @@ else:
     methodology_count = Counter(methodology_list)
     sorted_methodology_count = dict(sorted(methodology_count.items(), key=lambda x:x[1], reverse = True))
     st.bar_chart(sorted_methodology_count) 
+    ###Methodology (of other studies)
+    st.subheader('Methodology (of other studies)')
+    data = papers_assessed_df_included[papers_assessed_df_included['revmaster_study_type'] != 'Empirical']
+    methodology_o_list = data.revmaster_methodology.values.tolist()
+    methodology_o_count = Counter(methodology_o_list)
+    sorted_methodology_o_count = dict(sorted(methodology_o_count.items(), key=lambda x:x[1], reverse = True))
+    st.bar_chart(sorted_methodology_o_count)    
 
 ## sidebar#######################
   ###sidebar 
